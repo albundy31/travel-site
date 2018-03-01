d9bf57e74b323c39a1eedc1392552f4b8b9bccdc
albundy31 <rudolf.babarczi@gmail.com>
2018-03-01 11:16:16 +0100

    gulp & css files are organized

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp"),
+	postcss = require("gulp-postcss"),
+	autoprefixer = require("autoprefixer"),
+	cssvars = require("postcss-simple-vars"),
+	nested = require("postcss-nested"),
+	postcssimport = require("postcss-import"),
+	mixins = require("postcss-mixins");
+	
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([postcssimport, mixins, cssvars, nested, autoprefixer]))
+		.on('error', function(errorMessage) {
+			console.log(errorMessage.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
